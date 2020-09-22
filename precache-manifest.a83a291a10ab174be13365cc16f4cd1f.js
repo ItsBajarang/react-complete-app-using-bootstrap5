2626963aacaf7a99184ef354a284ce6b5d26779b
itsbajarang <bajarangjadhav2019@gmail.com>
2020-09-22 21:05:16 +0530

    Updates

---
+++ precache-manifest.a83a291a10ab174be13365cc16f4cd1f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14d49d7166e405d661ae7052d19b12f4",
+    "url": "/react-complete-app-using-bootstrap5/index.html"
+  },
+  {
+    "revision": "de3fe0d9adbc4f456d44",
+    "url": "/react-complete-app-using-bootstrap5/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "871cb7b2a7c38309c05f",
+    "url": "/react-complete-app-using-bootstrap5/static/css/main.d7084c92.chunk.css"
+  },
+  {
+    "revision": "de3fe0d9adbc4f456d44",
+    "url": "/react-complete-app-using-bootstrap5/static/js/2.e9736cb3.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/react-complete-app-using-bootstrap5/static/js/2.e9736cb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "871cb7b2a7c38309c05f",
+    "url": "/react-complete-app-using-bootstrap5/static/js/main.274f1e8c.chunk.js"
+  },
+  {
+    "revision": "edf534e5558a38dd098a",
+    "url": "/react-complete-app-using-bootstrap5/static/js/runtime-main.ec4d08ba.js"
+  },
+  {
+    "revision": "488a1443af9e39d0a449cdaf3f8e7d97",
+    "url": "/react-complete-app-using-bootstrap5/static/media/android.488a1443.svg"
+  },
+  {
+    "revision": "b3727bbd3b8a1c848600ad1d5f4ee9d9",
+    "url": "/react-complete-app-using-bootstrap5/static/media/flag.b3727bbd.svg"
+  },
+  {
+    "revision": "0716aba552638647f2b204c07f736b1c",
+    "url": "/react-complete-app-using-bootstrap5/static/media/global-network.0716aba5.svg"
+  },
+  {
+    "revision": "e9a248b4b6e701c247644937e7c533ac",
+    "url": "/react-complete-app-using-bootstrap5/static/media/mobile-app.e9a248b4.svg"
+  },
+  {
+    "revision": "71a7fc7d155fc5cd972f856ffdb4a5e4",
+    "url": "/react-complete-app-using-bootstrap5/static/media/network.71a7fc7d.svg"
+  },
+  {
+    "revision": "e2f7de8b1d662f52c7409059ac845efb",
+    "url": "/react-complete-app-using-bootstrap5/static/media/python.e2f7de8b.svg"
+  },
+  {
+    "revision": "eab76719fd611cab5eecb1eb1427675a",
+    "url": "/react-complete-app-using-bootstrap5/static/media/react.eab76719.svg"
+  },
+  {
+    "revision": "dde8bff33933455020fe4a37a61486db",
+    "url": "/react-complete-app-using-bootstrap5/static/media/search.dde8bff3.svg"
+  },
+  {
+    "revision": "66fe63746b1e8e6f468052d8fb01d7fd",
+    "url": "/react-complete-app-using-bootstrap5/static/media/youtube.66fe6374.svg"
+  }
+]);
